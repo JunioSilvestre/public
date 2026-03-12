bd0d0e24572e583da54b2bbaeb560c72090d84a3
Junio Silvestre <jcs@jcscode.com>
2026-03-12 18:00:15 -0400

    feat: configure tailwindcss and vercel deployment

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,10 @@
 import type { AppProps } from 'next/app';
+import '../styles/globals.css';
 
 export default function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <div className="mx-auto max-w-[1440px]">
+      <Component {...pageProps} />
+    </div>
+  );
 }
